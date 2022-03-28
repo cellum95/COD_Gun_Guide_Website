9244f47a7a6fe54f1b04fb07c1be689e47736892
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-28 18:42:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset = "utf-8">
+    	<meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    	<title>COD Gun Guide</title>
+    	<meta name = "description" content = "The main page of the COD Gun Guide website">
+		<link href = "CSS/style.php" rel = "stylesheet" type = "text/css">
+
+		<style>
+
+
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<nav class = "navbar">
+			<ul class = "navbar-nav">
+
+				<li class = "navbar-item">
+					<a href = "" class = "navbar-link"  style = "margin-left: 1750px;">Sign In</a>
+				</li>
+				<li class = "navbar-item">
+					<a href = "" class = "navbar-link" style = "margin-left: 15px;">Sign Out</a>
+				</li>
+
+			</ul>
+		</nav>
+
+		<header class = "header-title">
+			<h1>Call of Duty Gun Guide</h1>
+		</header>
+
+		<main>
+
+			<div class = "div-container">
+				<div class = "div-child one">
+					<form action = "#">
+						<label for = "guns">Select Gun: </label>
+						<select name = "guns" id = "guns">
+							<option value = "gun1">--</option>
+
+						</select>
+					</form>
+
+				</div>
+
+				<div class = "div-child two">
+					<form action = "#">
+						<label for = "attachment1">Select Attachment: </label>
+						<select name = "attachment1" id = "attachment1">
+							<option value = "attach1">--</option>
+
+						</select>
+					</form>
+
+
+					<br>
+
+					<form action = "#">
+						<label for = "attachment2">Select Attachment: </label>
+						<select name = "attachment2" id = "attachment2">
+							<option value = "attach1">--</option>
+
+						</select>
+					</form>
+
+					<br>
+
+					<form action = "#">
+						<label for = "attachment3">Select Attachment: </label>
+						<select name = "attachment3" id = "attachment3">
+							<option value = "attach1">--</option>
+
+						</select>
+					</form>
+				</div>
+			</div>
+
+		</main>
+
+		
+
+	</body>
+
+</html>
